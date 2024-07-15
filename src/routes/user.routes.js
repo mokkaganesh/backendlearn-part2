ebb329987a867b8d34b5e97e22afadff52e6a8b7
Ganesh <2110080050@klh.edu.in>
2024-07-15 18:37:02 +0530

    chore: Add new user routes and controller methods
    This commit adds new routes and controller methods to the user.routes.js and user.controller.js files. The added routes include endpoints for changing the user's password, getting the current user's details, updating the user's account details, updating the user's avatar and cover image, getting the user's channel profile, and retrieving the user's watch history. These routes are secured and require authentication using JSON Web Tokens (JWT).

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import {Router} from 'express';
-import { logoutUser, registerUser ,loginUser } from '../controllers/user.controller.js';
+import { logoutUser, registerUser ,loginUser, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage, getUserChannelProfile, getWatchHistory } from '../controllers/user.controller.js';
 import {upload} from '../middlewares/multer.middleware.js';
 import { verifyJWT } from '../middlewares/auth.middleware.js';
 
@@ -15,6 +15,19 @@ router.route('/register').post(
 
 router.route('/login').post(loginUser);
 
+//secured routes
 router.route('/logout').post(verifyJWT,logoutUser);
+router.route('/change-password').post(verifyJWT,changeCurrentPassword);
+router.route('/current-user').get(verifyJWT,getCurrentUser);
+router.route('/update-profile').patch(verifyJWT,updateAccountDetails);
+router.route('/update-avatar').patch(verifyJWT,upload.single('avatar'),updateUserAvatar);
+
+// updateUserAvatar
+router.route('/update-cover-image').patch(verifyJWT,upload.single('coverImage'),updateUserCoverImage);
+//getUserChannelProfile
+router.route('/getUserChannelProfile/:username').get(verifyJWT,getUserChannelProfile);
+
+router.route('/history').get(verifyJWT,getWatchHistory);
+
 
 export default router;
